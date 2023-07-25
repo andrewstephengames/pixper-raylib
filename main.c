0db9ec5c189983561b57dec40b6b705a62d70430
andrewstephengames <andrewstephengames999@gmail.com>
2023-07-25 18:39:01 +0300

    Refactored code base to fit the newly working main menu. Optimized DrawBackground().

--- main.c
+++ main.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h> //for strcat, strcpy and strlen
+#include <string.h> //for strcat, strcpy, strlen and strcmp
 #include <math.h> //for sqrt and pow (collisions)
 #include <time.h> //for clock() (random seed)
 #include <sqlite3.h> //for db
 #include <raylib.h>
+#include <raymath.h> // for Vector2Add
 
 #define PLATFORM_DESKTOP
 #define E_SZ 100
@@ -87,77 +88,23 @@ int callback(void *NotUsed, int argc, char **argv, char **azColName);
 void Commandline (int argc, char **argv);
 bool IsCollision (Entity *a, Entity *b, float c);
 void SetDifficulty (bool difficulty);
-void GenerateEntities (void);
 Vector2 CenterText (const char *s, int size, Vector2 pos);
 VectorPair DrawTextButton (const char *s, int size, Vector2 pos, int offset, Color bg, Color fg);
 void DrawBackground (float alpha);
-void InitMenu (void);
-void DrawMenu (void);
-void InitGame (void);
+void DrawMenu (int argc, char **argv);
+void InitMenu (int argc, char **argv);
 void PlayerMovement (void);
 void EnemyMovement (void);
+void GenerateEntities (void);
+void DrawEntities (void);
+void CalcCollisions (void);
+void DrawHUD (int argc, char **argv);
+void Gameplay (int argc, char **argv);
 
 int main (int argc, char **argv)
 {
-     if (rc != SQLITE_OK)
-     {
-          fprintf (stderr, "Cannot open the database: %s\n", sqlite3_errmsg(db));
-          sqlite3_close(db);
-          return 1;
-     }
-     Commandline(argc, argv);
-     InitGame();
-     while (!WindowShouldClose())
-     {
-          w.x = GetScreenWidth();
-          w.y = GetScreenHeight();
-          if (!game.mutemusic)
-               UpdateMusicStream(music[0]);
-          PlayerMovement();
-          EnemyMovement();
-          CalcCollisions();
-          BeginDrawing();
-               ClearBackground(BLACK);
-               DrawBackground();
-               DrawEntities();
-               DrawHUD();
-          EndDrawing();
-          if (IsWindowResized()) //|| IsWindowMaximized()) //TODO: Maximized
-          {
-               GenerateEntities();
-               DrawEntities();
-               DrawHUD();
-          }
-          if (IsKeyPressed (KEY_Q) || game.close)
-               break;
-          if (IsKeyPressed (KEY_F))
-               printf ("(%.3f %.3f)\n", player[0].x, player[0].y);
-     }
-     UnloadTexture (player[0].sprite);
-     UnloadTexture (background.sprite);
-     for (int i = 1; i < 100; i++)
-          if (grass[i].num || apple[i].num || bomb[i].num || tree[i].num)
-          {
-               UnloadTexture (grass[i].sprite);
-               UnloadTexture (apple[i].sprite);
-               UnloadTexture (bomb[i].sprite);
-               UnloadTexture (tree[i].sprite);
-          }
-     StopMusicStream(music[0]);
-     CloseAudioDevice();
+     InitMenu(argc, argv);
      CloseWindow();
-     strcpy (sql, "SELECT Name, Score, Difficulty FROM Players ORDER BY Score DESC");
-     rc = sqlite3_exec (db, sql, callback, 0, &err_msg); 
-     strcpy (sql, "SELECT Playername, RNG, Name FROM Obstacles ORDER BY RNG DESC LIMIT 3");
-     rc = sqlite3_exec (db, sql, callback, 0, &err_msg); 
-     if (rc != SQLITE_OK)
-     {
-          fprintf (stderr, "Failed to select data\n");
-          fprintf (stderr, "SQL Error: %s\n", err_msg);
-          sqlite3_close(db);
-          return 1;
-     }
-     sqlite3_close(db);
      return 0;
 }
 
@@ -279,6 +226,151 @@ void SetDifficulty (bool difficulty)
      bomb[0].num = GetRandomValue (w.y/b, w.x/b);
 }
 
+Vector2 CenterText (const char *s, int size, Vector2 pos)
+{
+     Vector2 newpos = {
+          .x = pos.x/2 - MeasureText (s, size)/2,
+          .y = pos.y/2 - size/2,
+     };
+     return newpos;
+}
+
+VectorPair DrawTextButton (const char *s, int textsize, Vector2 pos, int offset, Color bg, Color fg)
+{
+     Vector2 center = CenterText (s, textsize, pos);
+     Vector2 rect = {
+          .x = pos.x/10,
+          .y = pos.y/10,
+     };
+     size_t num = strlen(s);
+     if (num <= 4)
+          rect.x += MeasureText ("a", textsize);
+     else while (num > strlen(s)/2)
+          {
+               rect.x += MeasureText ("a", textsize);
+               --num;
+          }
+     DrawRectangle (center.x, center.y+offset, rect.x, rect.y, bg);
+     center.x += MeasureText ("a", textsize)/4;
+     DrawText (s, center.x, center.y+offset, textsize, fg);
+     center.y += offset;
+     VectorPair v = {
+          .a = center,
+          .b = Vector2Add (center, rect),
+     };
+     return v;
+}
+
+void DrawBackground (float alpha)
+{
+     Texture2D sprite = LoadTexture ("res/images/grass.png");
+     Color c = WHITE;
+     c.a = alpha;
+     Rectangle rect = {
+          .width = w.x,
+          .height = w.y,
+     };
+     SetShapesTexture (sprite, rect);
+     DrawRectangle (0, 0, w.x, w.y, c);
+}
+
+void DrawMenu (int argc, char **argv)
+{
+     int size = w.x/10;
+     char s[30];
+     strcpy (s, "Pixper");
+     Vector2 mid = CenterText (s, size, w);
+     DrawText (s, mid.x, mid.y/2, size, YELLOW);
+     size = w.x/20;
+     Vector2 input = GetMousePosition();
+     Color fg = YELLOW, bg = { 40, 40, 40, 120 };
+     strcpy (s, "Play");
+     buttons[PLAY] = DrawTextButton (s, size, w, 0, bg, fg);
+     if (input.x >= buttons[PLAY].a.x && input.x <= buttons[PLAY].b.x &&
+          input.y >= buttons[PLAY].a.y && input.y <= buttons[PLAY].b.y)
+          if (IsMouseButtonReleased(MOUSE_LEFT_BUTTON))
+               Gameplay(argc, argv);
+     strcpy (s, "Options");
+     buttons[OPTIONS] = DrawTextButton (s, size, w, w.x*0.08f, bg, fg);
+     if (input.x >= buttons[OPTIONS].a.x && input.x <= buttons[OPTIONS].b.x &&
+          input.y >= buttons[OPTIONS].a.y && input.y <= buttons[OPTIONS].b.y)
+          if (IsMouseButtonReleased(MOUSE_LEFT_BUTTON))
+               //TODO: options menu
+               printf ("Options\n");
+     strcpy (s, "Quit");
+     buttons[QUIT] = DrawTextButton (s, size, w, w.x*0.16f, bg, fg);
+     if (input.x >= buttons[QUIT].a.x && input.x <= buttons[QUIT].b.x &&
+          input.y >= buttons[QUIT].a.y && input.y <= buttons[QUIT].b.y)
+          if (IsMouseButtonReleased(MOUSE_LEFT_BUTTON))
+               game.close = 1;
+}
+
+void InitMenu (int argc, char **argv)
+{
+     SetConfigFlags (FLAG_WINDOW_RESIZABLE);
+     InitWindow (w.x, w.y, "Pixper");
+     SetTargetFPS(60);
+     SetTraceLogLevel(LOG_ERROR);
+     while (!WindowShouldClose() && !game.close)
+     {
+          w.x = GetRenderWidth();
+          w.y = GetRenderHeight();
+          BeginDrawing();
+               ClearBackground (BLACK);
+               DrawBackground (150);
+               DrawMenu(argc, argv);
+          EndDrawing();
+               if (IsKeyPressed(KEY_Q))
+                    break;
+     }
+}
+
+void PlayerMovement (void)
+{
+     bool k[4] = {
+          IsKeyDown(KEY_W) || IsKeyDown(KEY_UP),
+          IsKeyDown(KEY_A) || IsKeyDown(KEY_LEFT),
+          IsKeyDown(KEY_S) || IsKeyDown(KEY_DOWN),
+          IsKeyDown(KEY_D) || IsKeyDown(KEY_RIGHT),
+     };
+     if (k[0])
+          player[0].y -= player[0].speed;
+     if (k[1])
+          player[0].x -= player[0].speed;
+     if (k[2])
+          player[0].y += player[0].speed;
+     if (k[3])
+          player[0].x += player[0].speed;
+     if (player[0].x <= 5 && player[0].x != -1)
+          player[0].x = 5;
+     if (player[0].y <= 5 && player[0].y != -1)
+          player[0].y = 5;
+     if (player[0].x >= w.x-32)
+          player[0].x = w.x-32;
+     if (player[0].y >= w.y-32)
+          player[0].y = w.y-32;
+}
+
+void EnemyMovement (void)
+{
+     if (player[1].x < player[0].x)
+          player[1].x += player[1].speed;
+     else player[1].x -= player[1].speed;
+
+     if (player[1].y < player[0].y)
+          player[1].y += player[1].speed;
+     else player[1].y -= player[1].speed;
+
+     if (player[1].x <= 5 && player[1].x != -1)
+          player[1].x = 5;
+     if (player[1].y <= 5 && player[1].y != -1)
+          player[1].y = 5;
+     if (player[1].x >= w.x-32)
+          player[1].x = w.x-32;
+     if (player[1].y >= w.y-32)
+          player[1].y = w.y-32;
+}
+
 void GenerateEntities (void)
 {
      //Designate the first value of obstacle arrays to the number of obstacles
@@ -337,101 +429,6 @@ void GenerateEntities (void)
      }
 }
 
-void InitGame (void)
-{
-     SetConfigFlags(FLAG_WINDOW_RESIZABLE);
-     InitWindow(w.x, w.y, "Pixper");
-     InitAudioDevice();
-     SetTargetFPS(60);
-     SetTraceLogLevel(LOG_ERROR);
-     SetRandomSeed(clock());
-     rc = sqlite3_open ("res/db/stats.db", &db);
-     strcpy (sql, "CREATE TABLE IF NOT EXISTS Players (Name TEXT PRIMARY KEY, Score INTEGER, Difficulty BOOLEAN);"
-           "CREATE TABLE IF NOT EXISTS Obstacles (Playername TEXT, RNG INTEGER PRIMARY KEY, Name TEXT);");
-     
-     rc = sqlite3_exec(db, sql, 0, 0, &err_msg); //update db
-
-     game.health = 10;
-     game.delay = 0x1000*GetFrameTime();
-     game.difficulty = 0;
-     
-     sound[1] = LoadSound ("res/sounds/oof.ogg");
-     sound[2] = LoadSound ("res/sounds/eat.ogg");
-     sound[3] = LoadSound ("res/sounds/boom.ogg");
-
-     size_t soundsize = sizeof(sound) / sizeof (Sound) - 1;
-     for (size_t i = 1; i <= soundsize; i++)
-          SetSoundVolume(sound[i], 0.10f);
-
-     music[1] = LoadMusicStream("res/music/music1.ogg");     
-     music[2] = LoadMusicStream("res/music/music2.ogg");     
-     music[3] = LoadMusicStream("res/music/music3.ogg");     
-
-     size_t musicindex, musicsize;
-     musicsize = sizeof(music) / sizeof(Music);
-     musicindex = GetRandomValue (1, musicsize-1);
-     music[0] = music[musicindex];
-
-     PlayMusicStream (music[0]);
-     SetMusicVolume (music[0], 0.25f);
-
-     GenerateEntities();
-}
-
-void PlayerMovement (void)
-{
-     bool k[4] = {
-          IsKeyDown(KEY_W) || IsKeyDown(KEY_UP),
-          IsKeyDown(KEY_A) || IsKeyDown(KEY_LEFT),
-          IsKeyDown(KEY_S) || IsKeyDown(KEY_DOWN),
-          IsKeyDown(KEY_D) || IsKeyDown(KEY_RIGHT),
-     };
-     if (k[0])
-          player[0].y -= player[0].speed;
-     if (k[1])
-          player[0].x -= player[0].speed;
-     if (k[2])
-          player[0].y += player[0].speed;
-     if (k[3])
-          player[0].x += player[0].speed;
-     if (player[0].x <= 5 && player[0].x != -1)
-          player[0].x = 5;
-     if (player[0].y <= 5 && player[0].y != -1)
-          player[0].y = 5;
-     if (player[0].x >= w.x-32)
-          player[0].x = w.x-32;
-     if (player[0].y >= w.y-32)
-          player[0].y = w.y-32;
-}
-
-void EnemyMovement (void)
-{
-     if (player[1].x < player[0].x)
-          player[1].x += player[1].speed;
-     else player[1].x -= player[1].speed;
-
-     if (player[1].y < player[0].y)
-          player[1].y += player[1].speed;
-     else player[1].y -= player[1].speed;
-
-     if (player[1].x <= 5 && player[1].x != -1)
-          player[1].x = 5;
-     if (player[1].y <= 5 && player[1].y != -1)
-          player[1].y = 5;
-     if (player[1].x >= w.x-32)
-          player[1].x = w.x-32;
-     if (player[1].y >= w.y-32)
-          player[1].y = w.y-32;
-}
-
-Window CenterText (const char *s, int size)
-{
-     return (Window) {
-          .x = w.x/2 - MeasureText (s, size)/2,
-          .y = w.y/2 - size/2,
-     };
-}
-
 void DrawEntities (void)
 {
      for (int i = 1; i <= apple[0].num; i++)
@@ -446,21 +443,13 @@ void DrawEntities (void)
      {
           char s[] = "Hidden apples have been revealed!\0";
           int size = w.x/20;
-          Window mid = CenterText (s, size);
+          Vector2 mid = CenterText (s, size, w);
           DrawText (s, mid.x, mid.y, size, YELLOW);
           for (int i = 1; i <= apple[0].num; i++)
                DrawTexture (apple[i].sprite, apple[i].x, apple[i].y, WHITE);
      }
      DrawTexture (player[0].sprite, player[0].x, player[0].y, WHITE);
      DrawTexture (player[1].sprite, player[1].x, player[1].y, WHITE);
-}
-
-void DrawBackground (void)
-{
-     background.sprite = LoadTexture ("res/images/grass.png");
-     for (int i = 0; i < w.x; i += 256)
-          for (int j = 0; j < w.y; j += 256)
-               DrawTexture (background.sprite, i, j, WHITE);
 }
 
 void CalcCollisions (void)
@@ -527,7 +516,7 @@ void CalcCollisions (void)
      }
 }
 
-void DrawHUD (void)
+void DrawHUD (int argc, char **argv)
 {
      char buffer[101], s[30];
      snprintf (buffer, 30, "%s: %d", "Score", game.score);
@@ -539,23 +528,126 @@ void DrawHUD (void)
           game.health = 0;
           strcpy (s, "You Died!");
           int size = 50;
-          Window mid = CenterText (s, size);
+          Vector2 mid = CenterText (s, size, w);
           DrawText (s, mid.x, mid.y, size, GRAY);
           game.close = 1;
           sprintf (buffer, "INSERT OR REPLACE INTO Players VALUES ('%s', '%d', '%d');", player[1].name, game.score, game.difficulty);
           strcpy (sql, buffer);
           rc = sqlite3_exec(db, sql, 0, 0, &err_msg);
           //TODO: make end game menu
+          InitMenu(argc, argv);
      }
      if (game.score == apple[0].num)
      {
           strcpy (s, "You won!");
           int size = 50;
-          Window mid = CenterText (s, size);
+          Vector2 mid = CenterText (s, size, w);
           DrawText (s, mid.x, mid.y, size, YELLOW);
           game.close = 1;
           sprintf (buffer, "INSERT OR REPLACE INTO Players VALUES ('%s', '%d', '%d');", player[0].name, game.score, game.difficulty);
           strcpy (sql, buffer);
           rc = sqlite3_exec(db, sql, 0, 0, &err_msg);
+          //TODO: make end game menu
+          InitMenu(argc, argv);
      }
+}
+
+void Gameplay (int argc, char **argv)
+{
+     SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+     InitWindow(w.x, w.y, "Pixper");
+     InitAudioDevice();
+     SetTargetFPS(60);
+     SetTraceLogLevel(LOG_ERROR);
+     SetRandomSeed(clock());
+     rc = sqlite3_open ("res/db/stats.db", &db);
+     strcpy (sql, "CREATE TABLE IF NOT EXISTS Players (Name TEXT PRIMARY KEY, Score INTEGER, Difficulty BOOLEAN);"
+           "CREATE TABLE IF NOT EXISTS Obstacles (Playername TEXT, RNG INTEGER PRIMARY KEY, Name TEXT);");
+     
+     rc = sqlite3_exec(db, sql, 0, 0, &err_msg); //update db
+
+     game.health = 10;
+     game.delay = 0x1000*GetFrameTime();
+     game.difficulty = 0;
+     
+     sound[1] = LoadSound ("res/sounds/oof.ogg");
+     sound[2] = LoadSound ("res/sounds/eat.ogg");
+     sound[3] = LoadSound ("res/sounds/boom.ogg");
+
+     size_t soundsize = sizeof(sound) / sizeof (Sound) - 1;
+     for (size_t i = 1; i <= soundsize; i++)
+          SetSoundVolume(sound[i], 0.10f);
+
+     music[1] = LoadMusicStream("res/music/music1.ogg");     
+     music[2] = LoadMusicStream("res/music/music2.ogg");     
+     music[3] = LoadMusicStream("res/music/music3.ogg");     
+
+     size_t musicindex, musicsize;
+     musicsize = sizeof(music) / sizeof(Music);
+     musicindex = GetRandomValue (1, musicsize-1);
+     music[0] = music[musicindex];
+
+     PlayMusicStream (music[0]);
+     SetMusicVolume (music[0], 0.25f);
+
+     GenerateEntities();
+     if (rc != SQLITE_OK)
+     {
+          fprintf (stderr, "Cannot open the database: %s\n", sqlite3_errmsg(db));
+          sqlite3_close(db);
+          return;
+     }
+     Commandline(argc, argv);
+     while (!WindowShouldClose())
+     {
+          w.x = GetScreenWidth();
+          w.y = GetScreenHeight();
+          if (!game.mutemusic)
+               UpdateMusicStream(music[0]);
+          PlayerMovement();
+          EnemyMovement();
+          CalcCollisions();
+          BeginDrawing();
+               ClearBackground(BLACK);
+               DrawBackground(255);
+               DrawEntities();
+               DrawHUD(argc, argv);
+          EndDrawing();
+          if (IsWindowResized()) //|| IsWindowMaximized()) //TODO: Maximized
+          {
+               GenerateEntities();
+               DrawEntities();
+               DrawHUD(argc, argv);
+          }
+          if (IsKeyPressed (KEY_Q) || game.close)
+               break;
+          if (IsKeyPressed (KEY_F))
+               printf ("(%.3f %.3f)\n", player[0].x, player[0].y);
+     }
+     UnloadTexture (player[0].sprite);
+     UnloadTexture (background.sprite);
+     for (int i = 1; i < 100; i++)
+          if (grass[i].num || apple[i].num || bomb[i].num || tree[i].num)
+          {
+               UnloadTexture (grass[i].sprite);
+               UnloadTexture (apple[i].sprite);
+               UnloadTexture (bomb[i].sprite);
+               UnloadTexture (tree[i].sprite);
+          }
+     StopMusicStream(music[0]);
+     CloseAudioDevice();
+     CloseWindow();
+     strcpy (sql, "SELECT Name, Score, Difficulty FROM Players ORDER BY Score DESC");
+     rc = sqlite3_exec (db, sql, callback, 0, &err_msg); 
+     strcpy (sql, "SELECT Playername, RNG, Name FROM Obstacles ORDER BY RNG DESC LIMIT 3");
+     rc = sqlite3_exec (db, sql, callback, 0, &err_msg); 
+     if (rc != SQLITE_OK)
+     {
+          fprintf (stderr, "Failed to select data\n");
+          fprintf (stderr, "SQL Error: %s\n", err_msg);
+          sqlite3_close(db);
+          return;
+     }
+     sqlite3_close(db);
+     InitMenu(argc, argv);
 }
